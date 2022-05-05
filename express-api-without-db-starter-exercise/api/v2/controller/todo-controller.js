e8558741921e4271bf40aa985cde01a405656d9f
daigor <daigor.rodrigues@gmail.com>
2022-05-05 15:40:38 +0100

    DR:added two methods

--- express-api-without-db-starter-exercise/api/v2/controller/todo-controller.js
+++ express-api-without-db-starter-exercise/api/v2/controller/todo-controller.js
@@ -2,7 +2,7 @@ const TodoNotFoundError = require('../errors/todo-not-found-error');
 const Todo = require('../model/todo')
 
 idCounter = 1;
-const todos = [{"id":1, "title":"Clean room", "description":"Remember to hoover and put clothes away"}]
+const todos = [{"id":1, "title":"Clean room", "description":"Remember to hoover and put clothes away", "tags": ["tag1" , "tag2" , "tag3"]}]
 
 module.exports = {
 
@@ -39,7 +39,6 @@ module.exports = {
          next(new TodoNotFoundError(id));
     },
 
-
     delete: (req, res, next) => {
         const id = req.params.id;
         const todo = todos.find(todo => todo.id == id);
@@ -51,6 +50,35 @@ module.exports = {
             return
         }
         next(new TodoNotFoundError(id));
+
+    },
+
+    addTag: (req, res, next) => {
+        const id = req.params.id;
+        const updates = req.body;
+        const todo = todos.find(todo => todo.id == id);
+
+        if (todo) {
+            const index = todos.indexOf(todo);
+            todo.tags.push(index,1);
+            res.status(200).json(todo);
+            return;    
+         }
+         next(new TodoNotFoundError(id));
+    },
+
+    removeTag: (req, res, next) => {
+        const id = req.params.id;
+        const updates = req.body;
+        const todo = todos.find(todo => todo.id == id);
+
+        if (todo) {
+            todo.todoname = updates.todoname;
+            todos.tagssplice(index,1);
+            res.status(200).json(todo);
+            return;    
+         }
+         next(new TodoNotFoundError(id));
     }
 }
 
